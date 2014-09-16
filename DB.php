59db486219528c1648c21de42c101297d9b73d14
slowknight <slowknight3@gmail.com>
2014-09-15 23:15:40 -0700

    Modified DB.php : Added getMetadata() and get() helper functions.

--- DB.php
+++ DB.php
@@ -4,11 +4,12 @@ class DB
 {   
     protected $_conn;
     protected $_query;
+    protected $_where = array();
 
     public function __construct($host, $dbname, $username, $passphrase)
     {
         try {
-            $str = "mysql:host=" . $host . ";dbname=" . $dbname;
+            $str = "mysql:host=$host;dbname=$dbname";
             $this->_conn = new PDO($str, $username, $passphrase);
         } catch (PDOException $e) {
             trigger_error($e->getMessage(), E_USER_ERROR);
@@ -19,7 +20,7 @@ class DB
      * Executes the passed in query and returns an array of rows
      * 
      * @param string $query Query string
-     * @return array Array of Results
+     * @return array Array of arrays
      */
     public function query($query)
     {
@@ -47,9 +48,49 @@ class DB
         return $results;
     }
     
-    public function get($tableName, $numRows)
+    /**
+     * Helper function which performs a SELECT statement on the passed in
+     * table and returns an array of rows
+     * 
+     * @param type $tableName Table name
+     * @param type $numRows Number of rows to be returned (optional)
+     * 
+     * @return array Array of arrays
+     */
+    public function get($tableName, $numRows = NULL)
     {
+        $this->_query = "SELECT * FROM $tableName";
         
+        if ( !empty($this->_where) ) {
+            $keys = array_keys($this->_where);
+            $col = $keys[0];
+            $this->_query .= " WHERE " . $col . " = ?";
+        }
+        
+        if ( gettype($numRows) === "integer" ) {
+            $this->_query .= " LIMIT " . $numRows;
+        }
+        
+        $stmt = $this->_prepareQuery();
+        
+        if ( $this->_where ) {
+            $stmt->bindParam(1, $this->_where[$col]);
+        }
+        
+        $stmt->execute();
+        
+        $results = $this->_bindResults($stmt);
+        return $results;
+    }
+    
+    public function whereClause($prop, $value)
+    {
+        $this->_where[$prop] = $value;
+    }
+    
+    public function getMetadata($tableName)
+    {
+        return $this->query("DESCRIBE $tableName");
     }
 
     public function __destruct() {
